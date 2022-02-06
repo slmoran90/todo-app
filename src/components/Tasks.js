001cb96ee98347ac7cb77f9ee317059da442d825
Sergio Moran <moransergio.01@gmail.com>
2022-02-05 21:00:32 -0300

    function to edit task done

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react'
 import { collection, onSnapshot } from 'firebase/firestore'
 import deleteTask from '../services/deleteTask'
@@ -6,7 +7,7 @@ import './styles.css'
 import { Card, CardHeader, CardBody, CardText, CardFooter, Button } from 'reactstrap'
 import swal from '../services/sweetalertConfig'
 
-const Task = () => {
+const Task = ({ setCurrentId }) => {
   const [tasksList, setTasksList] = useState([])
 
   const handleDelete = (taskTitle, id) => {
@@ -67,14 +68,22 @@ const Task = () => {
               </CardText>
             </CardBody>
             <CardFooter className='d-flex justify-content-end gap-2 pt-1 border-0 bg-white'>
-              <Button className='btn-custom-primary'>
-                Editar
+              <Button
+                className='btn-custom-primary d-flex gap-2 align-items-center'
+                onClick={ () => {
+                  setCurrentId(task.id)
+                }}
+              >
+                <span>Editar</span>
+                <i className='far fa-edit fa-lg'/>
               </Button>
               <Button
                 color='danger'
                 onClick={ () => handleDelete(task.title, task.id) }
+                className='d-flex gap-2 align-items-center'
               >
-                  Borrar
+                <span>Borrar</span>
+                <i className='far fa-trash-alt fa-lg'/>
               </Button>
             </CardFooter>
           </Card>
